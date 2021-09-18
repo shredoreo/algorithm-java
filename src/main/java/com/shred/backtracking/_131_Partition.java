a16b84efd3dfc064bd6c9bec88ac1fc232741747
shredoreo <shreddd@live.com>
2021-09-18 14:46:54 +0800

    x

---
+++ src/main/java/com/shred/backtracking/_131_Partition.java
@@ -0,0 +1,13 @@
+package com.shred.backtracking;
+
+import java.util.List;
+
+public class _131_Partition {
+
+    class Solution {
+        public List<List<String>> partition(String s) {
+
+
+        }
+    }
+}
--- src/main/java/com/shred/backtracking/_40_CombinationSum2.java
+++ src/main/java/com/shred/backtracking/_40_CombinationSum2.java
@@ -1,11 +1,97 @@
 package com.shred.backtracking;
 
-import java.util.List;
+import java.util.*;
 
+/*
+40. 组合总和 II
+给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+candidates 中的每个数字在每个组合中只能使用一次。
+
+注意：解集不能包含重复的组合。
+
+
+
+示例 1:
+
+输入: candidates = [10,1,2,7,6,1,5], target = 8,
+输出:
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+示例 2:
+
+输入: candidates = [2,5,2,1,2], target = 5,
+输出:
+[
+[1,2,2],
+[5]
+]
+
+
+提示:
+
+1 <= candidates.length <= 100
+1 <= candidates[i] <= 50
+1 <= target <= 30
+ */
 public class _40_CombinationSum2 {
     class Solution {
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+        int sum = 0;
+
         public List<List<Integer>> combinationSum2(int[] candidates, int target) {
 
+            Arrays.sort(candidates);
+
+            backTracing(candidates, target, 0, new boolean[candidates.length]);
+            return result;
         }
+
+        public void backTracing(int[] candidates, int target, int index, boolean[] flag) {
+
+            if (sum == target) {
+                result.add(new ArrayList<>(deque));
+                return;
+            }
+            if (sum > target) {
+                return;
+            }
+
+            // 在for循环这里剪枝  candidates[i]+sum<=target
+            for (int i = index; i < candidates.length && candidates[i] + sum <= target; i++) {
+                //数组排序的情况下，判断当前数值 是否在同一层被使用过了
+                if (i > 0 && candidates[i] == candidates[i - 1] && !flag[i - 1]) {
+                    continue;
+                }
+
+                flag[i] = true;
+                deque.push(candidates[i]);
+                sum += candidates[i];
+
+                backTracing(candidates, target, i + 1, flag);
+
+                flag[i] = false;
+                deque.poll();
+                sum -= candidates[i];
+            }
+
+        }
+        /*
+        执行用时：
+2 ms
+, 在所有 Java 提交中击败了
+99.57%
+的用户
+内存消耗：
+38.8 MB
+, 在所有 Java 提交中击败了
+23.68%
+的用户
+         */
     }
 }
